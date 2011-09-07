d2397fce8a42484fd94e6408cb8228bec54ba523
Florian Mauracher <florian_mauracher@genua.de>
2011-09-07 13:09:15 +0200

    Indention fixing

--- createlinks.sh
+++ createlinks.sh
@@ -4,32 +4,31 @@ mkdir -p ~/.oldconf
 
 #.bashrc
 if [ -f ~/.bashrc ]
-then cp ~/.bashrc ~/.oldconf/
+	then cp ~/.bashrc ~/.oldconf/
 fi
 ln -fs ~/.myconf/.bashrc ~/.bashrc
 
 #.gitconfig
 if [ -f ~/.gitconfig ]
-then cp ~/.gitconfig ~/.oldconf/
+	then cp ~/.gitconfig ~/.oldconf/
 fi
 ln -fs ~/.myconf/.gitconfig ~/.gitconfig
 
 #.ssh/config
 if [ -f ~/.ssh/config ]
-then cp ~/.ssh/config ~/.oldconf/
+	then cp ~/.ssh/config ~/.oldconf/
 fi
 mkdir -p ~/.ssh/
 ln -fs ~/.myconf/.ssh/config ~/.ssh/config
 
 #.vimrc
 if [ -f ~/.vimrc ]
-then cp ~/.vimrc ~/.oldconf/
+	then cp ~/.vimrc ~/.oldconf/
 fi
 ln -fs ~/.myconf/.vimrc ~/.vimrc
 
 #.vim
 if [ -d ~/.vim ]
-then cp ~/.vim ~/.oldconf/
+	then cp ~/.vim ~/.oldconf/
 fi
 ln -s ~/.myconf/.vim/ ~/.vim
-
