70ce4f602787a542a4d61c90bb4dc45816eecd2d
Florian Mauracher <florian.mauracher@gmx.net>
2011-10-11 18:43:25 +0200

    Fixing .vim directory linking when call muliple times and moving the backupdir

--- createlinks.sh
+++ createlinks.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-mkdir -p ~/.oldconf
+mkdir -p ~/.tmp/.oldconf
 mkdir -p ~/.tmp
 mkdir -p ~/.tmp/.vimbak
 mkdir -p ~/.tmp/.vimtmp
 
 #.bashrc
 if [ -f ~/.bashrc ]
-	then cp ~/.bashrc ~/.oldconf/
+	then cp ~/.bashrc ~/.tmp/.oldconf/
 fi
 ln -fs ~/.myconf/.bashrc ~/.bashrc
 
 #.gitconfig
 if [ -f ~/.gitconfig ]
-	then cp ~/.gitconfig ~/.oldconf/
+	then cp ~/.gitconfig ~/.tmp/.oldconf/
 fi
 ln -fs ~/.myconf/.gitconfig ~/.gitconfig
 
 #.ssh/config
 if [ -f ~/.ssh/config ]
-	then cp ~/.ssh/config ~/.oldconf/
+	mkdir -p ~/.tmp/.oldconf/.ssh
+	then cp ~/.ssh/config ~/.tmp/.oldconf/
 fi
 mkdir -p ~/.ssh/
 chmod 600 ~/.myconf/.ssh/config
@@ -27,19 +28,19 @@ ln -fs ~/.myconf/.ssh/config ~/.ssh/config
 
 #.tmux.conf
 if [ -f ~/.tmux.conf ]
-	then cp ~/.tmux.conf ~/.oldconf/
+	then cp ~/.tmux.conf ~/.tmp/.oldconf/
 fi
 ln -fs ~/.myconf/.tmux.conf ~/.tmux.conf
 
 #.vimrc
 if [ -f ~/.vimrc ]
-	then cp ~/.vimrc ~/.oldconf/
+	then cp ~/.vimrc ~/.tmp/.oldconf/
 fi
 ln -fs ~/.myconf/.vimrc ~/.vimrc
 
 #.vim
 if [ -a ~/.vim ] && !([ -h ~/.vim ])
-	then cp -r ~/.vim ~/.oldconf/
+	then cp -r ~/.vim ~/.tmp/.oldconf/
 	rm -r ~/.vim/
 fi
-ln -fs ~/.myconf/.vim ~/.vim
+ln -fns ~/.myconf/.vim ~/.vim
