6b2e66bac4896a505a9a63c6729fc3bb21d37984
Florian Mauracher <florian.mauracher@genua.de>
2013-08-19 17:28:26 +0200

    Add script to ignore changes to .gitconfig and .vim/bundle/perl-support/../Templates

---
+++ ignore-conf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+case "$1" in
+    i*)
+		git update-index --assume-unchanged .vim/bundle/perl-support.vim/perl-support/templates/Templates
+		git update-index --assume-unchanged .gitconfig
+        ;;
+    t*)
+		git update-index --no-assume-unchanged .vim/bundle/perl-support.vim/perl-support/templates/Templates
+		git update-index --no-assume-unchanged .gitconfig
+        ;;
+    *)
+        echo "$0 i(gnore) | t(rack)"
+        exit 1
+esac
