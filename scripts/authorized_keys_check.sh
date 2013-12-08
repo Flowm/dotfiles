990469bd5e1d5029ca4744d4199229d9085791f6
Florian Mauracher <florian.mauracher@gmx.net>
2013-12-08 09:34:11 +0100

    scripts: Extend authorized_keys management

---
+++ scripts/authorized_keys_check.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+diff -su /home/flow/.myconf/.ssh/a_keys/authorized_keys_flow /home/flow/.ssh/authorized_keys
+diff -su /home/flow/.myconf/.ssh/a_keys/authorized_keys_tun /home/tun/.ssh/authorized_keys
