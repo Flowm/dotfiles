009d1ff5886264af6edde696d2562f4a6d338fa7
Florian Mauracher <florian.mauracher@gmx.net>
2013-12-08 09:26:44 +0100

    scripts/copy_authorized_keys.sh: Fix permissions bug

--- scripts/copy_authorized_keys.sh
+++ scripts/copy_authorized_keys.sh
@@ -4,4 +4,4 @@ sudo cp /home/flow/.myconf/.ssh/a_keys/authorized_keys_flow /home/flow/.ssh/auth
 sudo chown flow:flow /home/flow/.ssh/authorized_keys
 
 sudo cp /home/flow/.myconf/.ssh/a_keys/authorized_keys_tun /home/tun/.ssh/authorized_keys
-sudo chown tun:tun /home/flow/.ssh/authorized_keys
+sudo chown tun:tun /home/tun/.ssh/authorized_keys
