3f6194dcb81518bf5233d249e88233e4315f016a
Florian Mauracher <florian.mauracher@gmx.net>
2014-07-30 00:19:35 +0200

    Move keys to seperate directory

--- scripts/authorized_keys_check.sh
+++ scripts/authorized_keys_check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-diff -su /home/flow/.ssh/authorized_keys /home/flow/.myconf/.ssh/a_keys/authorized_keys_flow
-diff -su /home/tun/.ssh/authorized_keys /home/flow/.myconf/.ssh/a_keys/authorized_keys_tun
+diff -su /home/flow/.ssh/authorized_keys /home/flow/.myconf/.keys/ssh_authorized_keys/authorized_keys_flow
+diff -su /home/tun/.ssh/authorized_keys /home/flow/.myconf/.keys/ssh_authorized_keys/authorized_keys_tun
--- scripts/authorized_keys_copy.sh
+++ scripts/authorized_keys_copy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo cp /home/flow/.myconf/.ssh/a_keys/authorized_keys_flow /home/flow/.ssh/authorized_keys
+sudo cp /home/flow/.myconf/.keys/ssh_authorized_keys/authorized_keys_flow /home/flow/.ssh/authorized_keys
 sudo chown flow:flow /home/flow/.ssh/authorized_keys
 
-sudo cp /home/flow/.myconf/.ssh/a_keys/authorized_keys_tun /home/tun/.ssh/authorized_keys
+sudo cp /home/flow/.myconf/.keys/ssh_authorized_keys/authorized_keys_tun /home/tun/.ssh/authorized_keys
 sudo chown tun:tun /home/tun/.ssh/authorized_keys
