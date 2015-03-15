5aef3947fca34ec3120a4215f1e54b25fb3e349d
Florian Mauracher <florian.mauracher@gmx.net>
2015-03-15 02:11:32 +0100

    .zsh: Add antigen submodule

--- tools/config-install.sh
+++ tools/config-install.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env bash
+set -eu
 
-set -eux
+if [ $# -ne 0 ]; then
+	set -x
+fi
 
-CONFDIR="$HOME/.myconf"
-CONFHOME="$CONFDIR/home"
-CONFTMP="$CONFDIR/tmp"
-NEWHOME="$HOME"
 
-# Link all files in CONFHOME
-find "$CONFHOME/" -mindepth 1 -maxdepth 1 -type f
-find "$CONFHOME/" -mindepth 1 -maxdepth 1 -type f -exec ln -fs {} "$NEWHOME/" \;
+conf_dir="$HOME/.myconf"
+conf_home="$conf_dir/home"
+conf_tmp="$conf_dir/tmp"
+targt_dir="$HOME"
 
-for DIR in ".vim" ".zsh"; do
-	TMP="$CONFTMP/$DIR"
-	mkdir -p "$TMP"
-	ln -fs "$TMP" "$NEWHOME/"
+echo "Link all files in conf_home"
+find "$conf_home/" -mindepth 1 -maxdepth 1 -type f
+find "$conf_home/" -mindepth 1 -maxdepth 1 -type f -exec ln -fs {} "$targt_dir/" \;
+
+echo "Create tmp dirs and link"
+for d in ".vim" ".zsh"; do
+	tmp_dir="$conf_tmp/$d"
+	mkdir -p "$tmp_dir"
+	echo "$tmp_dir"
+	ln -fs "$tmp_dir" "$targt_dir/"
+done
+ln -dfs "$conf_dir/contrib/antigen" "$conf_tmp/.zsh/"
+
+echo "Link all files and folders in subdirectories of conf_home"
+for d in $(find "$conf_home/" -mindepth 1 -maxdepth 1 -type d); do
+	new_dir="$targt_dir/$(basename $d)"
+	mkdir -p "$new_dir"
+	find "$d" -mindepth 1 -maxdepth 1
+	find "$d" -mindepth 1 -maxdepth 1 -exec ln -dfs {} "$new_dir/" \;
 done
 
-# Link all files and folders in subdirectories of CONFHOME
-for DIR in $(find "$CONFHOME/" -mindepth 1 -maxdepth 1 -type d); do
-	NEWDIR="$NEWHOME/$(basename $DIR)"
-	mkdir -p "$NEWDIR"
-	find "$DIR" -mindepth 1 -maxdepth 1
-	find "$DIR" -mindepth 1 -maxdepth 1 -exec ln -dfs {} "$NEWDIR/" \;
-done
+echo "Initializing submodules"
+cd "$conf_dir"
+git submodule update --init --recursive
