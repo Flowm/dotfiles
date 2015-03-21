f6bffe9f350b13453c090df2e33d80de7007032f
Florian Mauracher <florian.mauracher@gmx.net>
2015-03-21 22:41:39 +0100

    home: Fix config-install symlinks again

--- tools/config-install.sh
+++ tools/config-install.sh
@@ -12,8 +12,8 @@ conf_tmp="$conf_dir/tmp"
 targt_dir="$HOME"
 
 echo "Link all files in conf_home"
-find "$conf_home/" -mindepth 1 -maxdepth 1 -type f
-find "$conf_home/" -mindepth 1 -maxdepth 1 -type f -exec ln -fs {} "$targt_dir/" \;
+find "$conf_home" -mindepth 1 -maxdepth 1 -type f
+find "$conf_home" -mindepth 1 -maxdepth 1 -type f -exec ln -fs {} "$targt_dir/" \;
 
 echo "Create tmp dirs and link"
 for d in ".vim" ".zsh"; do
@@ -24,13 +24,15 @@ for d in ".vim" ".zsh"; do
 done
 
 echo "Link all files and folders in subdirectories of conf_home"
-for d in $(find "$conf_home/" -mindepth 1 -maxdepth 1 -type d); do
+for d in $(find "$conf_home" -mindepth 1 -maxdepth 1 -type d); do
 	new_dir="$targt_dir/$(basename $d)"
 	mkdir -p "$new_dir"
 	find "$d" -mindepth 1 -maxdepth 1
 	find "$d" -mindepth 1 -maxdepth 1 -exec ln -nfs {} "$new_dir/" \;
 done
 
+mkdir -p $conf_tmp/.vimbak
+mkdir -p $conf_tmp/.vimtmp
 $conf_dir/tools/config-genfallback.sh
 
 echo "Initializing submodules"
