3a685bc648b239ccd7dcfc78021efe992bc33766
Florian Mauracher <florian.mauracher@gmx.net>
2015-07-09 21:12:56 +0200

    tools: Update config push and unistall

--- tools/config-uninstall.sh
+++ tools/config-uninstall.sh
@@ -3,6 +3,27 @@ set -eu
 
 if [ $# -ne 0 ]; then
 	set -x
-	rm -rf .bash_logout .bash_profile .bashrc .gitconfig .tmux.conf .vim .vimrc .zsh .zshrc .shcfg .ssh/config .myconf/tmp
 fi
 
+files=(
+		'.bash_logout'
+		'.bash_profile'
+		'.bashrc'
+		'.gitconfig'
+		'.tmux.conf'
+		'.vim'
+		'.vimrc'
+		'.zsh'
+		'.zshrc'
+		'.ssh/config'
+		)
+
+cd $HOME
+for file in "${files[@]}"; do
+	if [ -L $file ]; then
+		rm $file
+	fi
+done
+
+rm -rf '.shcfg'
+rm -rf '.myconf/tmp'
