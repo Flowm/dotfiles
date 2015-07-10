3eb019b62e2985117667ca9e90e12929a9c15048
Florian Mauracher <florian.mauracher@gmx.net>
2015-07-10 12:32:43 +0200

    tools: fix exclude in config push

--- tools/config-push.sh
+++ tools/config-push.sh
@@ -33,7 +33,7 @@ for host in "${myhosts[@]}"; do
 	#TODO: Check if host is up (ping -c 1 -w 1 $host; if ($? != 0) ...)
 	echo "Pushing to $host:"
 	if [ !$NOACTION ]; then
-		rsync -av --delete --exclude 'history/' --exclude 'tmp/.vimtmp' --exclude 'tmp/.vimbak' ~/.myconf/ $host:.myconf/
+		rsync -av --delete --exclude '/history/' --exclude '/tmp/.vimtmp' --exclude '/tmp/.vimbak' ~/.myconf/ $host:.myconf/
 		if [ $SETLINKS ]; then
 			ssh $host ".myconf/tools/config-install.sh"
 		fi
