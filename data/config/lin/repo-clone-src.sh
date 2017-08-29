d29cb5178837eacc53efd8b878dddc84309bb63f
Florian Mauracher <florian.mauracher@gmx.net>
2017-08-29 06:14:36 +0200

    config: Update src repo clone script

--- data/config/lin/repo-clone-src.sh
+++ data/config/lin/repo-clone-src.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
-mkdir -p ~/dev/cloned
-cd ~/dev/cloned
+mkdir -p ~/dev/cloned && cd ~/dev/cloned
 
-# gdb plugins
 git clone https://github.com/torvalds/linux.git
 git clone https://github.com/qemu/qemu.git
 git clone https://github.com/systemd/systemd.git
