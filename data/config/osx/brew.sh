f9305e60dc9c6e3400b0d4ea7df90dd65d89da10
Florian Mauracher <florian.mauracher@gmx.net>
2017-09-15 23:01:27 +0200

    instdb: Add brew pkg list

--- data/config/osx/brew.sh
+++ data/config/osx/brew.sh
@@ -1,5 +1,63 @@
 #!/usr/bin/env bash
 
+brew install \
+    arp-scan \
+    bash \
+    boost \
+    carthage \
+    cmake \
+    git \
+    htop-osx \
+    jq \
+    mobile-shell \
+    mtr \
+    picocom \
+    rename \
+    the_silver_searcher \
+    tmux \
+    unp \
+    watch \
+    wget \
+    youtube-dl \
+    zsh \
+    ;
+
+brew cask install \
+    1password \
+    aerial \
+    alfred \
+    appcleaner \
+    arduino \
+    arq \
+    bettertouchtool \
+    dash \
+    dropbox \
+    filezilla \
+    firefox \
+    flux \
+    google-chrome \
+    gource \
+    handbrake \
+    istat-menus \
+    iterm2 \
+    mactex \
+    macvim \
+    obs \
+    papers \
+    reflector \
+    skim \
+    skype \
+    spotify \
+    steam \
+    synergy \
+    teamspeak-client \
+    texpad \
+    tigervnc-viewer \
+    vagrant \
+    virtualbox \
+    vlc \
+    ;
+
 brew tap caskroom/fonts
 brew cask install font-yanone-kaffeesatz
 brew cask install font-inconsolata
