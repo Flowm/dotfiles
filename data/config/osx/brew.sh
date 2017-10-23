9c16f93cae576c6ce38143d058fdb05172ee42bb
Florian Mauracher <florian.mauracher@gmx.net>
2017-10-23 18:12:12 +0200

    config: Use array for brew app definition

--- data/config/osx/brew.sh
+++ data/config/osx/brew.sh
@@ -1,64 +1,72 @@
 #!/usr/bin/env bash
 
-brew install \
-    arp-scan \
-    bash \
-    boost \
-    carthage \
-    cmake \
-    git \
-    htop-osx \
-    jq \
-    mobile-shell \
-    mtr \
-    picocom \
-    rename \
-    the_silver_searcher \
-    tmux \
-    unp \
-    watch \
-    wget \
-    youtube-dl \
-    zsh \
-    ;
+echo "Installing brew apps"
+brewapps=(
+    arp-scan
+    bash
+    boost
+    carthage
+    cmake
+    git
+    htop-osx
+    jq
+    mobile-shell
+    mtr
+    picocom
+    rename
+    the_silver_searcher
+    tmux
+    unp
+    watch
+    wget
+    youtube-dl
+    zsh
+)
+brew install ${brewapps[@]}
 
-brew cask install \
-    1password \
-    aerial \
-    alfred \
-    appcleaner \
-    arduino \
-    arq \
-    bettertouchtool \
-    dash \
-    dropbox \
-    filezilla \
-    firefox \
-    flux \
-    google-chrome \
-    gource \
-    handbrake \
-    istat-menus \
-    iterm2 \
-    mactex \
-    macvim \
-    obs \
-    papers \
-    reflector \
-    skim \
-    skype \
-    spotify \
-    steam \
-    synergy \
-    teamspeak-client \
-    texpad \
-    tigervnc-viewer \
-    vagrant \
-    virtualbox \
-    vlc \
-    ;
+echo "Installing brew cask apps"
+brewcaskapps=(
+    1password
+    aerial
+    alfred
+    appcleaner
+    arduino
+    arq
+    bettertouchtool
+    dash
+    dropbox
+    filezilla
+    firefox
+    flux
+    google-chrome
+    gource
+    handbrake
+    istat-menus
+    iterm2
+    mactex
+    macvim
+    obs
+    papers
+    reflector
+    skim
+    skype
+    spotify
+    steam
+    synergy
+    teamspeak-client
+    texpad
+    tigervnc-viewer
+    vagrant
+    virtualbox
+    vlc
+)
+brew cask install ${brewcaskapps[@]}
 
+echo "Installing brew fonts"
 brew tap caskroom/fonts
-brew cask install font-yanone-kaffeesatz
-brew cask install font-inconsolata
-brew cask install font-droid-serif
+brewfonts=(
+    font-yanone-kaffeesatz
+    font-inconsolata
+    font-droid-serif
+)
+brew install ${brewfonts[@]}
