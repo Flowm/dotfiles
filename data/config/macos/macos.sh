59b306cff5cf7a5c43f1441743e3607908812435
Florian Mauracher <flo@mauracher.eu>
2021-11-08 07:13:30 +0100

    macos: Update hostname

--- data/config/macos/macos.sh
+++ data/config/macos/macos.sh
@@ -18,10 +18,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "mars1a"
-sudo scutil --set HostName "mars1a"
-sudo scutil --set LocalHostName "mars1a"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "mars1a"
+sudo scutil --set ComputerName "mars1p"
+sudo scutil --set HostName "mars1p"
+sudo scutil --set LocalHostName "mars1p"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "mars1p"
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
