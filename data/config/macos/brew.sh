4bf0992aeb3a38a3b625d333a071c40486ce6a5d
Florian Mauracher <flo@mauracher.eu>
2024-11-19 22:23:07 +0100

    macos: Update apps

--- data/config/macos/brew.sh
+++ data/config/macos/brew.sh
@@ -6,45 +6,62 @@ action=install
 
 echo "Installing brew apps"
 brew_apps=(
-    ansible
+    # ansible
+    # apngasm
     arp-scan
     bpytop
     brotli
+    colima
     coreutils
     direnv
+    docker
+    docker-completion
+    docker-compose
     dos2unix
     git
-    git-cal
+    # git-cal
+    # git-filter-repo
     git-lfs
-    git-quick-stats
+    # git-quick-stats
+    # git-sizer
     gnu-sed
-    go
+    # go
     gource
     htop
-    iftop
+    # iftop
+    imagemagick
     jq
+    languagetool
+    # mitmproxy
     mosh
-    mosquitto
+    # mosquitto
     mtr
     ncdu
+    neovim
     nmap
     node
     pandoc
-    picocom
+    # picocom
+    # pngcrush
+    # pngquant
+    # poetry
     pv
     pwgen
-    radare2
+    # radare2
     rename
     rsync
-    sl
+    sshpass
     sshuttle
     telnet
     tmux
+    tmuxinator
+    tmuxinator-completion
+    tree
     unp
     watch
     wget
     xz
-    youtube-dl
+    yt-dlp
     zsh
     zstd
 )
@@ -52,58 +69,75 @@ brew $action "${brew_apps[@]}"
 
 echo "Installing brew cask apps"
 brew_cask=(
-    aerial
-    airserver
+    1password
+    # airserver
     alfred
-    appcleaner
+    # appcleaner
     arq
     balenaetcher
-    betaflight-configurator
+    # betaflight-configurator
     bettertouchtool
+    blender
+    # chromium
     coconutbattery
     cyberduck
+    # db-browser-for-sqlite
     discord
-    docker
-    drawio
-    dropbox
+    # drawio
     firefox
-    flux
+    # flux
     geekbench
     google-chrome
-    google-drive-file-stream
+    # google-cloud-sdk
+    google-drive
     google-earth-pro
     grandperspective
+    gyroflow
     handbrake
+    # imhex
+    # inkscape
     istat-menus
     iterm2
-    jqbx
     ledger-live
+    losslesscut
+    # microsoft-teams
+    # moonlight
+    # ngrok
+    notion
+    # nvidia-geforce-now
     obs
+    # openaudible
     panoply
-    parallels
-    postman
+    # parallels
+    # postman
     qgis
     raindropio
+    # sidequest
     skim
     spotify
     steam
-    suspicious-package
-    teamviewer
-    texpad
-    tigervnc-viewer
+    # suspicious-package
+    # teamviewer
+    # temurin
+    # texpad
+    # tigervnc-viewer
+    # utm
     visual-studio-code
     vlc
-    wireshark
-    zotero
+    # webstorm
+    # whisky
+    # wireshark
+    # zoom
+    # zotero
 )
 brew install --cask "${brew_cask[@]}"
 
-echo "Installing brew cask driver apps"
-brew tap homebrew/cask-drivers
-brew_cask_drivers=(
-    saleae-logic
-)
-brew install "${brew_cask_drivers[@]}"
+#echo "Installing brew cask driver apps"
+#brew tap homebrew/cask-drivers
+#brew_cask_drivers=(
+#    saleae-logic
+#)
+#brew install "${brew_cask_drivers[@]}"
 
 #echo "Installing brew cask repair"
 #brew install vitorgalvao/tiny-scripts/cask-repair
